0ccbb494e401f092666edd283ac23d0a5c69ef89
Jackson Farnsworth <jfarnsworth13@IDC120-21.local>
2016-04-17 18:20:43 -0500

    changed type of description in ACOrg to text

--- sstation/grails-app/domain/sstation/CampusOrg.groovy
+++ sstation/grails-app/domain/sstation/CampusOrg.groovy
@@ -10,7 +10,7 @@ class CampusOrg {
 	
     static constraints = {
 		contactEmail(email:true,blank:false,nullable:false)
-		description(email:false,nullable:false)
+		description(email:false,nullable:false,type:text)
 		contact(blank:false,nullable:false)
 		contactPhone(blank:false,nullable:false)
 		name(blank:false,nullable:false)
