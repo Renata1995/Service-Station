c0b4ef46eaf8441a81f9a69922016dc464ae405f
Michael Higgs <mhiggs@austincollege.edu>
2016-04-23 18:26:02 -0500

    recovered commorg type mapping

--- sstation/grails-app/domain/sstation/CampusOrg.groovy
+++ sstation/grails-app/domain/sstation/CampusOrg.groovy
@@ -7,10 +7,13 @@ class CampusOrg {
 	String contactPhone
 	String contactEmail
 	
+	static mapping = {
+		description type: "text"
+	}
 	
     static constraints = {
 		contactEmail(email:true,blank:false,nullable:false)
-		description(email:false,nullable:false,type:"text")
+		description(email:false,nullable:false)
 		contact(blank:false,nullable:false)
 		contactPhone(blank:false,nullable:false)
 		name(blank:false,nullable:false)
