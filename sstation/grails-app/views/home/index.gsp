c6d84b2752e7d3e7c836d33df4fcbc35981540a9
Zixian Chai <zchai13@act024218.admin.austincollege.edu>
2016-05-01 16:42:39 -0500

    initial service hours by year station report

--- sstation/grails-app/views/home/index.gsp
+++ sstation/grails-app/views/home/index.gsp
@@ -56,7 +56,7 @@ div.col-md-7 {
 		</div>
 		<!-- homeCoreInfor -->
 
-		<div class="col-md-7">
+		<div class="col-md-7" id="homeTable">
 			<div id="homeTableName" class="formHeading">
 				<g:link action="pending" controller="hour">Pending Hours</g:link>
 			</div>
@@ -114,6 +114,13 @@ div.col-md-7 {
 	<!-- row -->
 
 	<g:render template="hourkpi" />
+	
+	<script>
+	 $(function(){
+		 var height=$("#homeTable").css("height")-$("#homeCoreInfor").css("padding");
+		 $("#homeCoreInfor").css("height",height);
+		 });
+	</script>
 
 </body>
 </html>
--- sstation/test/unit/sstation/ReportsControllerSpec.groovy
+++ sstation/test/unit/sstation/ReportsControllerSpec.groovy
@@ -7,6 +7,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ReportsController)
+@Mock([ServiceHour,StationReportService])
 class ReportsControllerSpec extends Specification {
 
     def setup() {
@@ -15,6 +16,21 @@ class ReportsControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test yearReport"() {
+		when:
+			controller.stationReportService=Mock(StationReportService)
+			controller.yearReport()
+		then:
+			view=="/reports/yearReport"
+			
     }
+	
+	void "test _hoursByYear"() {
+		when:
+			params.year="2016"
+			controller._hoursByYear()
+		then:
+			view=="/reports/_hoursByYear"
+		
+	}
 }
