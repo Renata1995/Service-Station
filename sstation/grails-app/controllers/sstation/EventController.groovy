cc846f1edc7be358a291d7630bbe6a927e2121f1
Todd McCormack <tmccormack12@idc120-05.local>
2016-05-04 11:25:32 -0500

    added admin and mod permissions to the correct pages

--- sstation/grails-app/controllers/sstation/EventController.groovy
+++ sstation/grails-app/controllers/sstation/EventController.groovy
@@ -4,6 +4,12 @@ import sstation.CampusOrg;
 import sstation.CommAg;
 import sstation.Event;
 import sstation.ServiceHour;
+import grails.transaction.Transactional
+import java.security.MessageDigest
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_ADMIN','ROLE_ADMIN'])
+@Transactional(readOnly = false)
 
 class EventController {
 	/*
