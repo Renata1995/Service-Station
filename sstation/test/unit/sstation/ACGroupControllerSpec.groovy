52b670ff04b92fb6e97576aa9d3e4f668f7b06a8
Todd McCormack <tmccormack12@IDC120-12.local>
2016-05-04 19:21:59 -0500

    fixed tests in ACGroup

--- sstation/test/unit/sstation/ACGroupControllerSpec.groovy
+++ sstation/test/unit/sstation/ACGroupControllerSpec.groovy
@@ -7,49 +7,74 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ACGroupController)
+@Mock([CampusOrg,ServiceHour])
 class ACGroupControllerSpec extends Specification {
 
-     void "test when successful save and save view shown"() {
-		
+	void "test when successful save and save view shown for the card"() {
+
 		when:
 		def co=Mock(CampusOrg)
 		controller._saveOnCard(co)
-		
+
 		then:
-		view=="/ACGroup/formSaved"
-    }
-	
-	void "test when not saved successfully and form view will be shown"(){
+		view=="/ACGroup/orgForm"
+		model.card==1
+	}
+
+	void "test when not saved successfully and form view will be shown for the card"(){
 		when:
 		def co=Mock(CampusOrg)
 		co.contactPhone="aaaa"
 		controller._saveOnCard(co)
-		
+
 		then:
 		view=="/ACGroup/orgForm"
 		model.form==1
 		model.card==1
 	}
-	
-	void "test that if an agency is not saved(_saveOnTable) successfully the form view will be rendered"(){
+
+	void "test when successful save and save view shown for the table"() {
+
 		when:
 		def co=Mock(CampusOrg)
-		co.contactPhone="aaaa"
-		controller._saveOnCard(co)
-		
+		controller._saveOnTable(co)
+
 		then:
 		view=="/ACGroup/orgForm"
-		model.form==1
+		model.table==1
 	}
-	
-	void "test when not saved successfully and save view will be shown" (){
+
+	void "test when not saved successfully and form view will be shown for the table" (){
 		when:
 		def co=Mock(CampusOrg)
 		co.contactEmail="aaaa"
-		controller._saveOnCard(co)
-		
+		controller._saveOnTable(co)
+
 		then:
-		view=="/ACGroup/formSaved"
+		view=="/ACGroup/orgForm"
 		model.form==1
+		model.table==1
+	}
+	
+	void "delete an agency on table"(){
+		when:
+		def org=Mock(CampusOrg)
+		org.save(flush:true)
+		controller._deleteOnTable(org)
+
+		then:
+		CampusOrg.count()==0
+		view=="/ACGroup/_orgTable"
+	}
+
+	void "delete an agency on Card"(){
+		when:
+		def org=Mock(CampusOrg)
+		org.save(flush:true)
+		controller._deleteOnCard(org)
+
+		then:
+		CampusOrg.count()==0
+		view=="/ACGroup/_orgCardMain"
 	}
 }
